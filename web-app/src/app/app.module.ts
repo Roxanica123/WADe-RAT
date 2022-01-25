370fdc25d730073208d988aec4e7dd00a125447f
Roxana Irimia <irimia.andreea.roxana@gmail.com>
2022-01-25 16:48:25 +0200

    add basic home page form

---
+++ web-app/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopHeaderComponent } from './pages/top-header/top-header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon'
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopHeaderComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ web-app/src/app/pages/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import Languages from '../../../assets/languages.json';
+
+interface Language {
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  public languages: Language[] = Languages.languages;
+  public selected = Languages.languages[0].value;
+
+  userForm = this.formBuilder.group({
+    link: ['', Validators.required],
+    language: ['', Validators.required],
+    text: ['', Validators.required],
+  });
+
+  constructor(private readonly formBuilder:FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.userForm.value);
+  }
+}
