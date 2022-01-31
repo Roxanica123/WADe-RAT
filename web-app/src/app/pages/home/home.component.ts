e6cd8662dc6407fa753218ad997fd8303712d076
Duarms <duarms@yahoo.com>
2022-01-31 23:06:19 +0200

    sexy front-end

--- web-app/src/app/pages/home/home.component.ts
+++ web-app/src/app/pages/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { RequestsService } from 'src/app/services/requests.service';
+import { SnackService } from 'src/app/services/snack.service';
+import { MatchReasponse, NoMatchReasponse } from 'src/app/types/dataType';
 import { environment } from 'src/environments/environment';
 import Languages from '../../../assets/languages.json';
 
@@ -18,6 +20,9 @@ interface Language {
 export class HomeComponent implements OnInit {
   public languages: Language[] = Languages.languages;
   public selected = Languages.languages[0].value;
+  public displayedColumns = ["Query"]
+  
+  public show_query:string[] = [];
 
   userForm = this.formBuilder.group({
     openApiDocumentUrl: ['', Validators.required],
@@ -26,11 +31,44 @@ export class HomeComponent implements OnInit {
   });
 
   constructor(private readonly formBuilder: FormBuilder,
-    private readonly requestsService: RequestsService) { }
+    private readonly requestsService: RequestsService,
+    private readonly snack: SnackService
+    ) { }
 
   ngOnInit(): void {
   }
-  async onSubmit() {
-    console.log(await this.requestsService.post(environment.gatewayURL, this.userForm.value));
+
+  public async onSubmit() {
+    const rez:any =  await this.requestsService.post<MatchReasponse | NoMatchReasponse>(environment.gatewayURL, this.userForm.value).catch ( (error) => {
+      this.snack.error("Ther has been a problem! " + error.message);
+    });
+
+    console.log(rez);
+    
+    if ( rez.url  ){
+
+      this.show_query = [rez.url];
+      console.log(this.show_query);
+    } 
+   
+    if ( rez.suggestions ){
+      if ( rez.suggestions.length == 0){
+        this.show_query = [];
+        this.snack.error("\t\tNo route found!\t\t");
+        return;
+      }
+
+      this.show_query = rez.suggestions;
+      console.log(this.show_query);
+    }
+
+
+     this.snack.info("\t\tSucces!\t\t");
+
+  }
+  
+
+  public clickMe(row:string):void{
+    console.log("row=>", row)
   }
 }
---
+++ web-app/src/app/services/snack.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class SnackService {
+
+    constructor(private _snackBar: MatSnackBar) { }
+
+    public info(message: string): void {
+        this._snackBar.open(message, "", {
+            duration: 4000,
+            panelClass: ["snack-info"]
+        });
+    }
+
+    public error(message: string): void {
+        this._snackBar.open(message, "", {
+            duration: 4000,
+            panelClass: ["snack-error"]
+        });
+    }
+}
